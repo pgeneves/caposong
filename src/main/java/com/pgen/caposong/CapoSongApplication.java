bfff6ffa5777d0d587445d69c7e740cf832a53c9
pgeneves <pgeneves@gmail.com>
2016-12-17 23:06:25 +0100

    service definition cleanup and resources loading for dev

--- src/main/java/com/pgen/caposong/CapoSongApplication.java
+++ src/main/java/com/pgen/caposong/CapoSongApplication.java
@@ -3,32 +3,36 @@ package com.pgen.caposong;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJSONDoc
-public class CapoSongApplication extends SpringBootServletInitializer {
+public class CapoSongApplication extends WebMvcConfigurerAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(CapoSongApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CapoSongApplication.class, args);
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CapoSongApplication.class);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/static/")
+                .setCachePeriod(0);
+        super.addResourceHandlers(registry);
     }
 
     @Bean
-    public HttpMessageConverters customConverters()
-    {
+    public HttpMessageConverters customConverters() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setDateFormat(new ISO8601DateFormat());
 
@@ -36,5 +40,11 @@ public class CapoSongApplication extends SpringBootServletInitializer {
         jackson.setObjectMapper(objectMapper);
 
         return new HttpMessageConverters(jackson);
+    }
+
+    // see https://stackoverflow.com/questions/27381781/java-spring-boot-how-to-map-my-my-app-root-to-index-html
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/index.html");
     }
 }
--- src/main/resources/static/core/translate/translate.service.js
+++ src/main/resources/static/core/translate/translate.service.js
@@ -1,49 +1,43 @@
 'use strict';
 
-angular.
-  module('core.translate').
-  factory('Lang', ['$http', '$q', '$timeout', function($http, $q, $timeout) {
-        var translate = {}
-        translate.$http = $http;
-        translate.$q = $q;
-        translate.$timeout =$timeout;
+function Translate_service($http, $q, $timeout) {
+        this.$http = $http;
+        this.$q = $q;
+        this.$timeout =$timeout;
+        this.langMapPromises = null;
+        this.langMap=null;
+        this.curLangKey=null;
+        this.curLang=null;
+        this.cachedText=null;
+        this.messages={};
 
         // Define all supported lang
-        translate.allLangs = [{label:'Français', key:'fr'},
+        this.allLangs = [{label:'Français', key:'fr'},
         {label:'Portuguêse', key:'pt'},
         {label:'English', key:'en'},];
 
-        translate.langMapPromises = null;
-        translate.langMap=null;
-        translate.curLangKey=null;
-        translate.curLang=null;
-        translate.cachedText=null;
-
-        translate.messages={};
-        console.debug("Instantiating Lang");
-
-        translate.getLangKey = function() {
+        this.getLangKey = function() {
           return this.curLangKey;
-        }.bind(translate)
+        };
 
-        translate.setLang = function(langKey) {
+        this.setLang = function(langKey) {
             this.curLangKey=langKey;
             this.curLang=this.langMap[langKey];
             this.messages=this.cachedText[this.curLang];
-        }.bind(translate)
+        };
 
-        translate._loadLangFile = function(deferred, lang) {
+        this._loadLangFile = function(deferred, lang) {
           this.$timeout(function() {
              this.$http.get("/lang/text_"+lang+".json")
                  .then(function(response) {
-                    console.log("Got en text");
-                    console.log(response.data);
+                    console.debug("Got en text");
+                    console.debug(response.data);
                     deferred.resolve(response.data)
                 });
             }.bind(this),500)
-          }.bind(translate)
+        };
 
-        translate._initialize = function() {
+        this._initialize = function() {
             this.langMapPromises = [];
             this.langMap = {};
             this.cachedText = {};
@@ -58,18 +52,24 @@ angular.
             }.bind(this));
             this.curLangKey='en';
             this.curLang=this.langMap['en'];
-        }.bind(translate)
+        };
 
-        translate._cacheText = function() {
+        this._cacheText = function() {
+          this._initialize();
           return this.$q.all(this.langMapPromises).then(function(results){
               return results;
           }.bind(this))
-        }.bind(translate)
+        };
+}
 
-        translate._initialize();
-        translate._cacheText().then(function(result) {
+angular.
+  module('core.translate').
+  factory('Lang', ['$http', '$q', '$timeout', function($http, $q, $timeout) {
+        console.debug("Instantiating Lang");
+        var service = new Translate_service($http, $q, $timeout);
+        service._cacheText().then(function(result) {
                this.cachedText = result;
                this.messages=this.cachedText[this.curLang];
-           }.bind(translate));;
-        return translate;
+           }.bind(service));;
+        return service;
   }]);
