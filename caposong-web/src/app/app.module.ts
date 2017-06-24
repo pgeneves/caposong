d60aec435dea8c087014e0c5293cbc7d74f900ca
pgeneves <pgeneves@gmail.com>
2017-06-24 10:13:08 +0200

    Migrate webapp to angular4 wip

--- caposong-web/src/app/app.module.ts
+++ caposong-web/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SongListModule} from './song-list/song-list.module';
+import {SongDetailsModule} from './song-details/song-details.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
